f23dbb08cae71e61a45134514ea023e9bfd8c71e
Fawzis651 <fawzisadiq900@icloud.com>
2025-05-06 14:01:41 +0100

    Update StaffHire.java

--- StaffHire.java
+++ StaffHire.java
@@ -2,75 +2,38 @@ public class StaffHire {
     private int vacancyNumber;
     private String designation;
     private String jobType;
-    private String staffNamee;
+    private String staffName;
     private String joiningDate;
     private String qualification;
     private String appointedBy;
     private boolean joined;
 
-    public StaffHire(int vacancyNumber, String designation, String jobType, String staffName, String joiningDate,
-                     String qualification, String appointedBy, boolean joined) {
+    public StaffHire(int vacancyNumber, String designation, String jobType, String staffName,
+                     String joiningDate, String qualification, String appointedBy, boolean joined) {
         this.vacancyNumber = vacancyNumber;
         this.designation = designation;
         this.jobType = jobType;
         this.staffName = staffName;
-        this.joiningDate = joining Date;
+        this.joiningDate = joiningDate;
         this.qualification = qualification;
         this.appointedBy = appointedBy;
         this.joined = joined;
     }
 
-    public int getVacancyNumber() {
-        return vacancyNumber;
-    }
+    public int getVacancyNumber() { return vacancyNumber; }
+    public String getDesignation() { return designation; }
+    public String getJobType() { return jobType; }
+    public String getStaffName() { return staffName; }
+    public String getJoiningDate() { return joiningDate; }
+    public String getQualification() { return qualification; }
+    public String getAppointedBy() { return appointedBy; }
+    public boolean getJoined() { return joined; }
 
-    public String getDesignation() {
-        return designation;
-    }
-
-    public String getJobType() {
-        return jobType;
-    }
-
-    public String getStaffName() {
-        return staffName;
-    }
-
-    public String getJoiningDate() {
-        return joiningDate;
-    }
-
-    public String getQualification() {
-        return qualification;
-    }
-
-    public String getAppointedBy() {
-        return appointedBy;
-    }
-
-    public boolean getJoined() {
-        return joined;
-    }
-
-    public void setStaffName(String name) {
-        staffName = name;
-    }
-
-    public void setJoiningDate(String date) {
-        joiningDate = date;
-    }
-
-    public void setQualification(String q) {
-        qualification = q;
-    }
-
-    public void setAppointedBy(String a) {
-        appointedBy = a;
-    }
-
-    public void setJoined(boolean j) {
-        joined = j;
-    }
+    public void setStaffName(String name) { staffName = name; }
+    public void setJoiningDate(String date) { joiningDate = date; }
+    public void setQualification(String q) { qualification = q; }
+    public void setAppointedBy(String a) { appointedBy = a; }
+    public void setJoined(boolean j) { joined = j; }
 
     public void showInfo() {
         System.out.println("Vacancy Number: " + vacancyNumber);
@@ -82,3 +45,4 @@ public class StaffHire {
         System.out.println("Appointed By: " + appointedBy);
         System.out.println("Joined: " + joined);
     }
+}
